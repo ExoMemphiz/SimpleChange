df0bd017926c1a12b28801574a5fa49a480882b8
CHRIS <CHRIS@192.168.1.10>
2016-04-21 16:47:21 +0200

    Added all drugs

---
+++ src/model/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CHRIS
+ */
+public class Player {
+ 
+    String name;
+    int money;
+    ArrayList<Drug> drugs;
+    
+}
---
+++ src/model/drug/AngelDust.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.drug;
+
+import model.Drug;
+
+/**
+ *
+ * @author CHRIS
+ */
+public class AngelDust implements Drug {
+
+    int price;
+
+    public AngelDust(int price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String getName() {
+        return "Heroin";
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+}
